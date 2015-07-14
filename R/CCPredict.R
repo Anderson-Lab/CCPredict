9a8511b4f01dc74bd3aca96b845068802f55802f
DavidEMoore <moorede@g.cofc.edu>
2015-07-14 14:57:05 -0400

    Update CCPredict.R

--- R/CCPredict.R
+++ R/CCPredict.R
@@ -42,9 +42,9 @@ optimize.cckopls <- function(X,ytr,L,noxRange,LambdaRange,kfold=2,cluster.size=8
 
   #deleted CCPredict package from foreach for testing
   print('optimizing nox...')
+  print(length(noxRange))
   kcauc <- foreach(i=1:length(noxRange),.packages=c('kernlab','AUC','kopls','CCPredict'),.combine=rbind) %dopar% {
   #for (i in 1:length(noxRange)){
-    print(i)
     n <- noxRange[i]
     kcauc.values <- c()
     for (j in 1:kfold){
